b456089f9ff1b4c987f7965b01ac553c6ac5f712
yunmi <yunmi@Z140C-XPRESS-F2>
2026-06-26 15:45:26 +0200

    Add project files.

---
+++ Cyberchatbot POE/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cyberchatbot_POE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
